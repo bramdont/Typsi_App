fc298dc1ae2863f42218f4dce3b5de782ca12047
Bramdont Garcia <bramdont@gmail.com>
2021-01-14 21:20:11 -0400

    Initial Commit

---
+++ Typsi App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Typsi App
+//
+//  Created by Bramdont José García Aponte on 14/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
